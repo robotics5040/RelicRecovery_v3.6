fa722240dd9b829db2bbfd9c0283e76787bf35c3
BiznisCat <breinhold@live.com>
2018-01-16 12:59:07 -0500

    push test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPull.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.RobotLog;
-
+//easter egg
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
