71d996260a666033112af5e401f32bbcc0111f83
Jed <jed.chancey@gmail.com>
2018-01-17 14:31:40 -0500

    Succesfully implemented the RelicDeliverySystem into OmniBot_Iterative2!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicDeliverySystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicDeliverySystem.java
@@ -12,22 +12,21 @@ public class RelicDeliverySystem {
 
     private HardwareOmniRobot robot;
 
-    double rwCurrent = robot.relicWrist.getPosition(), rwGoal = rwCurrent;
-
-    double power = 0.5;
     private final int RELIC_OUT = 3000; // Minimum Value to Prevent Over Extension
     private final int RELIC_IN = 0;
-    double SERVO_INCREMENT = 0.04, decay = 0.008;
+    private double increment = 0.04;
 
     public RelicDeliverySystem(HardwareOmniRobot robot) {
+        //Initialize the robot based on the given one
         this.robot = robot;
     }
 
     public void moveSlide(double joystick) {
+        double power = 0.5;
         int relicMotorPosition = robot.relicMotor.getCurrentPosition();
         int newRelicMotorPosition = relicMotorPosition;
 
-        if(joystick< -0.1&&robot.relicMotor.getCurrentPosition() <RELIC_OUT) {
+        if(joystick< -0.1 && robot.relicMotor.getCurrentPosition() <RELIC_OUT) {
             robot.relicStopper.setPosition(0.3);
             newRelicMotorPosition = RELIC_OUT;
             power = 1.0;
@@ -41,7 +40,6 @@ public class RelicDeliverySystem {
         newRelicMotorPosition = Range.clip(newRelicMotorPosition, RELIC_IN, RELIC_OUT);
         robot.relicMotor.setTargetPosition(newRelicMotorPosition);
         relicMotorPosition = robot.relicMotor.getCurrentPosition();
-        //power = Math.abs(right_stick_y_2);
 
         if(power < 0.4){
             power = 0.4;
@@ -51,31 +49,37 @@ public class RelicDeliverySystem {
         robot.relicMotor.setPower(power);
     }
 
-        if(robot.relicWrist.getPosition() < 0.30){
-        SERVO_INCREMENT = 0.01;
-    }else{
-        SERVO_INCREMENT = 0.04;
-    }
+    public void moveWrist(double joystick){
+        double rwCurrent = robot.relicWrist.getPosition(), rwGoal = rwCurrent;
 
-        if(left_stick_y_2 < -0.1){
-        rwGoal = rwCurrent + SERVO_INCREMENT;
-        //SERVO_INCREMENT -= decay;
-    }else if(left_stick_y_2 > 0.1){
-        rwGoal = rwCurrent - SERVO_INCREMENT;
-        //SERVO_INCREMENT += decay;
-    }
+        if(robot.relicWrist.getPosition() < 0.30){
+            increment = 0.01;
+        }else{
+            increment = 0.04;
+        }
+
+        if(joystick < -0.1){
+            rwGoal = rwCurrent + increment;
+        }else if(joystick > 0.1){
+            rwGoal = rwCurrent - increment;
+        }
 
         if(rwGoal > 1.0){
-        rwGoal = 1.0;
-    }else if(rwGoal < 0.0){
-        rwGoal = 0.0;
-    }
-
-        if(a_button_2){
-        robot.relicClaw.setPosition(0.0);
-    }else{
-        robot.relicClaw.setPosition(0.5);
-    }
+            rwGoal = 1.0;
+        }else if(rwGoal < 0.0){
+            rwGoal = 0.0;
+        }
 
         robot.relicWrist.setPosition(rwGoal);
+    }
+
+    public void openClaw(boolean button) {
+        if (button) {
+            robot.relicClaw.setPosition(0.0);
+        } else {
+            robot.relicClaw.setPosition(0.5);
+        }
+    }
+
+
 }
