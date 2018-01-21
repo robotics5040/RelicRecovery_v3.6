1804ae94a76273300cd45c2d57767bae01685c8f
Jed <jed.chancey@gmail.com>
2018-01-20 21:21:56 -0500

    Created the Drive Train class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by jedch on 1/20/2018.
+ */
+
+public class DriveTrain {
+
+    private HardwareOmniRobot robot;
+
+    public DriveTrain(HardwareOmniRobot robot){
+        this.robot = robot;
+    }
+
+    public void makeAdjustments()
+
+}
