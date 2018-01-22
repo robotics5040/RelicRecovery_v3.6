d75a9383dee74205c61350406b2327ef03239a72
Jed <jed.chancey@gmail.com>
2018-01-21 19:49:10 -0500

    Continued to work on the DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.RobotLog;
+
 /**
  * Created by jedch on 1/20/2018.
  */
@@ -8,18 +10,54 @@ public class DriveTrain {
 
     private HardwareOmniRobot robot;
 
+    private double forward, side, rotation;
+    private boolean toggle = true;
+    private enum Side {FRONT, LEFT, RIGHT, BACK}
+    private Side currentSide = Side.FRONT;
+    public static final String MESSAGETAG = "5040MSG";
+
     public DriveTrain(HardwareOmniRobot robot){
         this.robot = robot;
     }
 
-    public void makeAdjustments(boolean dup, boolean ddown, boolean dleft, boolean dright){
+    public void makeAdjustments (boolean dup, boolean ddown, boolean dleft, boolean dright){
         //convert the booleans to integers.
-        int forward  = dup ? 1 : 0;
-        int backward = ddown ? 1 : 0;
-        int left     = dleft ? 1 : 0;
-        int right    = dright ? 1 : 0;
+        int up    = dup ? 1 : 0;
+        int back  = ddown ? 1 : 0;
+        int left  = dleft ? 1 : 0;
+        int right = dright ? 1 : 0;
 
+        side    = (-0.5 * left) + (0.5 * right);
+        forward = (-0.5 * up) + (0.5 * back);
+    }
+
+    public void changeSide (boolean front, boolean right, boolean left, boolean back) {
+        if (front) {
+            currentSide = Side.FRONT;
+        } else if (right) {
+            currentSide = Side.RIGHT;
+        } else if (left) {
+            currentSide = Side.LEFT;
+        } else if (back) {
+            currentSide = Side.BACK:
+        }
+    }
+
+    public void drive () {
 
     }
+
+    public void omniDrive(double sideways, double forward, double rotation){
+        try {
+            robot.leftMotor1.setPower(robot.limit(((forward - sideways)/2) * 1 + (-.2 * rotation)));
+            robot.leftMotor2.setPower(robot.limit(((forward + sideways)/2) * 1 + (-.2 * rotation)));
+            robot.rightMotor1.setPower(robot.limit(((-forward - sideways)/2) * 1 + (-.2 * rotation)));
+            robot.rightMotor2.setPower(robot.limit(((-forward + sideways)/2) * 1 + (-.2 * rotation)));
+        } catch (Exception e) {
+            RobotLog.ee(MESSAGETAG, e.getStackTrace().toString());
+        }
+    }
+
+
 
 }
